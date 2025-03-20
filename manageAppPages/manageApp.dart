96d53af2354c73dde5b6c01c37d6f4ceda762954
armwark75 <aekkasit.sak@student.mahidol.ac.th>
2025-03-20 21:56:28 +0700

    Update manageApp.dart

--- manageAppPages/manageApp.dart
+++ manageAppPages/manageApp.dart
@@ -22,103 +22,139 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  int selectedIndex = 0;
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 5, vsync: this);
+  }
+
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 5, // ใช้ 5 Tabs
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color(0xFF205568),
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Image.asset('assets/PlaySphere_Logo.png', height: 80.h),
-              Row(
-                children: [
-                  Text("Account", style: TextStyle(color: Colors.white)),
-                  SizedBox(width: 5.w),
-                  Icon(Icons.account_circle, color: Colors.white),
-                ],
-              ),
-            ],
-          ),
-          bottom: TabBar(
-            isScrollable: true,
-            indicatorColor: Colors.white,
-            labelColor: Colors.white,
-            unselectedLabelColor: Colors.grey,
-            tabs: [
-              Tab(icon: Icon(Icons.home), text: "Home"),
-              Tab(icon: Icon(Icons.settings), text: "ManageApp"),
-              Tab(icon: Icon(Icons.search), text: "Search"),
-              Tab(icon: Icon(Icons.person), text: "MyProfile"),
-              Tab(icon: Icon(Icons.shopping_cart), text: "MyCart"),
-            ],
-          ),
-        ),
-        body: Row(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF205568),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Expanded(
-              flex: 2,
-              child: Container(
-                color: Colors.grey.shade300,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 10.h),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 10.w),
-                      child: Text(
-                        "MyApp",
-                        style: TextStyle(
-                          fontSize: 18.sp,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 10.h),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: List.generate(3, (index) => GameCard()),
-                    ),
-                    SizedBox(height: 20.h),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 10.w),
-                      child: Text(
-                        "History added myGames",
-                        style: TextStyle(
-                          fontSize: 18.sp,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 10.h),
-                    GameHistoryTable(),
-                  ],
-                ),
-              ),
+            Image.asset('assets/PlaySphere_Logo.png', height: 80.h),
+            Row(
+              children: [
+                Text("Account", style: TextStyle(color: Colors.white)),
+                SizedBox(width: 5.w),
+                Icon(Icons.account_circle, color: Colors.white),
+              ],
             ),
           ],
         ),
+        bottom: TabBar(
+          controller: _tabController,
+          isScrollable: true,
+          indicatorColor: Colors.white,
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.grey,
+          tabs: [
+            Tab(icon: Icon(Icons.home), text: "Home"),
+            Tab(icon: Icon(Icons.settings), text: "ManageApp"),
+            Tab(icon: Icon(Icons.search), text: "Search"),
+            Tab(icon: Icon(Icons.person), text: "MyProfile"),
+            Tab(icon: Icon(Icons.shopping_cart), text: "MyCart"),
+          ],
+        ),
       ),
-    );
-  }
-}
-
-class MenuItem extends StatelessWidget {
-  final IconData icon;
-  final String text;
-
-  MenuItem({required this.icon, required this.text});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: Icon(icon, color: Colors.white),
-      title: Text(text, style: TextStyle(color: Colors.white)),
-      tileColor: Colors.grey.shade800,
-      onTap: () {},
+      body: Row(
+        children: [
+          Container(
+            width: 50.w,
+            color: Colors.grey.shade800,
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () => onItemTapped(0),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 10.h),
+                    width: double.infinity,
+                    color:
+                        selectedIndex == 0
+                            ? Colors.grey.shade600
+                            : Colors.transparent,
+                    child: Column(
+                      children: [
+                        Icon(Icons.apps, color: Colors.white),
+                        Text("app", style: TextStyle(color: Colors.white)),
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => onItemTapped(1),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 10.h),
+                    width: double.infinity,
+                    color:
+                        selectedIndex == 1
+                            ? Colors.grey.shade600
+                            : Colors.transparent,
+                    child: Column(
+                      children: [
+                        Icon(Icons.add, color: Colors.white),
+                        Text("add", style: TextStyle(color: Colors.white)),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(10.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "MyApp",
+                    style: TextStyle(
+                      fontSize: 18.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: List.generate(3, (index) => GameCard()),
+                  ),
+                  SizedBox(height: 20.h),
+                  Text(
+                    "History added myGames",
+                    style: TextStyle(
+                      fontSize: 18.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10.h),
+                  Expanded(child: GameHistoryTable()),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -143,14 +179,14 @@ class GameCard extends StatelessWidget {
 class GameHistoryTable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 10.w),
+    return SingleChildScrollView(
       child: Table(
         border: TableBorder.all(),
         columnWidths: {
-          0: FractionColumnWidth(0.2),
-          1: FractionColumnWidth(0.5),
-          2: FractionColumnWidth(0.3),
+          0: FractionColumnWidth(0.1),
+          1: FractionColumnWidth(0.4),
+          2: FractionColumnWidth(0.2),
+          3: FractionColumnWidth(0.3),
         },
         children: [
           TableRow(
@@ -159,7 +195,7 @@ class GameHistoryTable extends StatelessWidget {
               TableCell(child: Center(child: Text("ลำดับ"))),
               TableCell(child: Center(child: Text("ชื่อ"))),
               TableCell(child: Center(child: Text("ราคา"))),
-              TableCell(child: Center(child: Text(''))),
+              TableCell(child: Center(child: Text(""))),
             ],
           ),
           TableRow(
