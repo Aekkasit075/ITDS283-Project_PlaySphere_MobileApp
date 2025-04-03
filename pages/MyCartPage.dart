c8d647a7f4f0be921388d0e1da103d4173940496
armwark75 <aekkasit.sak@student.mahidol.ac.th>
2025-04-03 21:09:32 +0700

    Update MyCartPage.dart

--- pages/MyCartPage.dart
+++ pages/MyCartPage.dart
@@ -4,16 +4,24 @@ import 'HomePage.dart';
 import 'ManageApp.dart';
 import 'SearchPage.dart';
 import 'MyProfilePage.dart';
+import 'futures/CheckOutPage.dart';
 
 class MyCartPage extends StatefulWidget {
   @override
   _MyCartPageState createState() => _MyCartPageState();
 }
 
-class _MyCartPageState extends State<MyCartPage> with SingleTickerProviderStateMixin {
-  int selectedIndex = 4; 
+class _MyCartPageState extends State<MyCartPage>
+    with SingleTickerProviderStateMixin {
+  int selectedIndex = 4;
   late TabController _tabController;
   bool isLoading = true;
+  List<Map<String, dynamic>> cartItems = [
+    {"name": "สินค้า 1", "price": 69.00, "details": "พื้นที่ 500 MB"},
+    {"name": "สินค้า 2", "price": 138.00, "details": "พื้นที่ 1 GB"},
+  ];
+
+  double get totalPrice => cartItems.fold(0, (sum, item) => sum + item["price"]);
 
   @override
   void initState() {
@@ -34,7 +42,7 @@ class _MyCartPageState extends State<MyCartPage> with SingleTickerProviderStateM
   Future<void> _loadImage() async {
     await Future.delayed(Duration(milliseconds: 500));
     setState(() {
-      isLoading = false; 
+      isLoading = false;
     });
   }
 
@@ -70,10 +78,83 @@ class _MyCartPageState extends State<MyCartPage> with SingleTickerProviderStateM
     }
   }
 
+  void _DeleteAllSuccessDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Icon(Icons.check_circle, color: Colors.green, size: 30),
+              const SizedBox(height: 10),
+              const Text(
+                'ลบรถเข็นทั้งหมดสำเร็จ',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pop(context); 
+                },
+                child: const Text('OK', style: TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _DeleteAllCartDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Row(
+            children: [
+              Icon(Icons.warning, color: Colors.red),
+              SizedBox(width: 10),
+              Text("ยืนยันการลบ"),
+            ],
+          ),
+          content: Text("คุณยืนยันที่จะลบรถเข็นทั้งหมดหรือไม่?"),
+          actions: [
+            TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.grey),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("No", style: TextStyle(color: Colors.white)),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.blue),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _DeleteAllSuccessDialog();
+              },
+              child: Text("Yes", style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: const Color(0xFF205568),
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -104,9 +185,260 @@ class _MyCartPageState extends State<MyCartPage> with SingleTickerProviderStateM
           ],
         ),
       ),
-      body: isLoading
-          ? Center(child: CircularProgressIndicator()) 
-          : Center(child: Text('MyCart Page Content')), 
+      body:
+          isLoading
+              ? Center(child: CircularProgressIndicator())
+              : Container(
+                color: const Color(0xFF205568),
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10.h,
+                        horizontal: 16.w,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "รถเข็นของฉัน",
+                            style: TextStyle(
+                              fontSize: 18.sp,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Text(
+                            "จำนวนตะกร้า: ${cartItems.length}",
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 10.h),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => CheckOutPage(),
+                              ),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFFA2A2A2),
+                          ),
+                          child: Text(
+                            "ชำระเงินทั้งหมด",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        Text(
+                          ":รวม THB${totalPrice.toStringAsFixed(2)}",
+                          style: TextStyle(
+                            fontSize: 12.sp,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: _DeleteAllCartDialog,
+                          child: Text(
+                            "ลบตะกร้าทั้งหมด",
+                            style: TextStyle(
+                              color: Colors.red,
+                              decoration: TextDecoration.underline,
+                              decorationColor: Colors.red,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 8.h),
+                    if (cartItems.isNotEmpty)
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Color(0xFF796F6F),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: ListView.builder(
+                          itemCount: cartItems.length,
+                          itemBuilder: (context, index) {
+                            return _buildCartItem(cartItems[index]);
+                          },
+                        ),
+                      ),
+                    )
+                  else
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          "ไม่มีสินค้าในรถเข็น",
+                          style: TextStyle(color: Colors.white, fontSize: 18.sp),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+    );
+  }
+
+  void _DeleteCartDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Row(
+            children: [
+              Icon(Icons.warning, color: Colors.red),
+              SizedBox(width: 10),
+              Text("ยืนยันการลบ"),
+            ],
+          ),
+          content: Text("คุณยืนยันที่จะลบรถเข็นนี้หรือไม่?"),
+          actions: [
+            TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.grey),
+              onPressed: () {
+                Navigator.of(context).pop(); // ปิด Dialog ถ้ากด No
+              },
+              child: Text("No", style: TextStyle(color: Colors.white)),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.blue),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _showSuccessDeleteDialog();
+              },
+              child: Text("Yes", style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showSuccessDeleteDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Icon(Icons.check_circle, color: Colors.green, size: 30),
+              const SizedBox(height: 10),
+              const Text(
+                'ลบรถเข็นสำเร็จ',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('OK', style: TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildCartItem(Map<String, dynamic> item) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8.h),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(width: 80.w, height: 80.h, color: Colors.grey[300]),
+              SizedBox(width: 10.w),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      item["name"],
+                      style: TextStyle(
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(item["details"]),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 5.h),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Text(
+              "THB${item["price"]}",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 16.sp,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: 10.h),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CheckOutPage()),
+                  );
+                },
+                child: Text("ชำระเงิน"),
+              ),
+              GestureDetector(
+                onTap: _DeleteCartDialog,
+                child: Text(
+                  "ลบรายการ",
+                  style: TextStyle(
+                    color: Colors.red,
+                    decoration: TextDecoration.underline,
+                    decorationColor: Colors.red,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
